f485f8f11151ccd130d73f285e3579130856d035
steven.yan <75616468+YZY-stack@users.noreply.github.com>
2022-11-17 21:45:44 +0800

    add resnest and convnext

--- disfin.py
+++ disfin.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 from model.xception import Xception
+from model.resnest import Resnest
+# from model.efficient import Efficient
 from cunet import Conditional_UNet as CUNet
 import math
 import random
@@ -59,7 +61,12 @@ class disfin(nn.Module):
         self.do = nn.Dropout(0.2)
 
         # models
-        self.encoder = self.init_xcep()
+        # self.encoder = self.init_xcep()  # xception
+        self.encoder = self.init_resnest()  # resnest
+        # self.encoder = self.init_efficient()  # efficient
+        self.encoder = self.init_convnext()  # convnext
+
+
         self.con_gan = CUNet(2)
         # self.block_fake = MLP(in_f=1024, hidden_dim=512, out_f=512)
         # self.block_real = MLP(in_f=1024, hidden_dim=512, out_f=512)
@@ -88,10 +95,35 @@ class disfin(nn.Module):
         state_dict = {k:v for k, v in state_dict.items() if 'fc' not in k}
         xcep.load_state_dict(state_dict, False)
         return xcep
-    
+
+    def init_resnest(self):
+        from resnest.torch import resnest50
+        resnest_layer = resnest50(pretrained=True)
+        net = Resnest(resnest_layer, num_classes=self.num_classes)
+        return net
+
+    def init_efficient(self):
+        from efficientnet_pytorch import EfficientNet
+        model = EfficientNet.from_pretrained('efficientnet-b7')
+        net = Efficient(model, num_classes=self.num_classes)
+        return net
+
+    def init_convnext(self):
+        from timm.models import create_model
+        net = create_model(
+        'convnext_base', 
+        pretrained=True, 
+        num_classes=2, 
+        drop_path_rate=0.0,
+        # layer_scale_init_value=1e-6,
+        head_init_scale=1.0,
+        )
+        return net
+
     def forward(self, cat_data, train=True, pair_index=0):
         bs = cat_data.shape[0]
-        hidden = self.encoder.features(cat_data)  # -> bs,1024
+        # hidden = self.encoder.features(cat_data)  # -> bs,1024
+        hidden = self.encoder.forward_features(cat_data)  # -> bs,1024
         # out = self.head(hidden)
         hidden_real = hidden[:bs//2, :, :, :]
         hidden_fake = hidden[bs//2:, :, :, :]
@@ -121,7 +153,8 @@ class disfin(nn.Module):
             if pair_index == 0:
                 # reconstruction mse loss
                 forgery_image_12 = self.con_gan(f1, c2)
-                hidden_fake_plus = self.encoder.features(forgery_image_12)
+                # hidden_fake_plus = self.encoder.features(forgery_image_12)
+                hidden_fake_plus = self.encoder.forward_features(forgery_image_12)
                 c3, f3 = hidden_fake_plus[:, :self.encoder_feat_dim, :, :], hidden_fake[:, self.encoder_feat_dim:, :, :]
                 f3_spe, f3_share = f3[:, :self.half_fingerprint_dim, :, :], f2[:, self.half_fingerprint_dim:, :, :]
 
@@ -132,7 +165,8 @@ class disfin(nn.Module):
             else:
                 # reconstruction mse loss
                 forgery_image_12 = self.con_gan(f2, c1)
-                hidden_fake_plus = self.encoder.features(forgery_image_12)
+                # hidden_fake_plus = self.encoder.features(forgery_image_12)
+                hidden_fake_plus = self.encoder.forward_features(forgery_image_12)
                 c3, f3 = hidden_fake_plus[:, :self.encoder_feat_dim, :, :], hidden_fake[:, self.encoder_feat_dim:, :, :]
                 f3_spe, f3_share = f3[:, :self.half_fingerprint_dim, :, :], f2[:, self.half_fingerprint_dim:, :, :]
 
@@ -140,8 +174,10 @@ class disfin(nn.Module):
                 reconstruction_image_1 = self.con_gan(f1, c3)
 
             # head for spe and sha
-            out_spe = self.head_spe(torch.cat((f2_spe, f1_spe, f3_spe), dim=0))
-            out_sha = self.head_sha(torch.cat((f2_share, f1_share, f3_share), dim=0))
+            # out_spe = self.head_spe(torch.cat((f2_spe, f1_spe, f3_spe), dim=0))
+            # out_sha = self.head_sha(torch.cat((f2_share, f1_share, f3_share), dim=0))
+            out_spe = self.head_spe(torch.cat((f2_spe, f1_spe), dim=0))
+            out_sha = self.head_sha(torch.cat((f2_share, f1_share), dim=0))
             out = (out_spe, out_sha)
 
             return None, (out, reconstruction_image_1, reconstruction_image_2, forgery_image_12)
