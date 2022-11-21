ac8e21e263da4ecb2ac1002e7df3bc45ad21d14d
steven.yan <75616468+YZY-stack@users.noreply.github.com>
2022-11-21 23:28:16 +0800

    Add TTA for testing

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import torch
 import os
 import cv2
 import albumentations as A
+from albumentations import DualTransform
+import ttach as tta
 import numpy as np
 import random
 from torch.utils import data
@@ -10,6 +12,7 @@ from sklearn.metrics import average_precision_score, precision_recall_curve, acc
 from sklearn.metrics import roc_curve
 from sklearn.metrics import auc as cal_auc
 from PIL import Image
+from more_itertools import chunked
 import sys
 import logging
 
@@ -17,50 +20,86 @@ import logging
 fake_dict= {'Deepfakes': 1, 'Face2Face': 2, 'FaceSwap': 3, 'NeuralTextures': 4}
 
 
+def isotropically_resize_image(img, size, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC):
+    h, w = img.shape[:2]
+    if max(w, h) == size:
+        return img
+    if w > h:
+        scale = size / w
+        h = h * scale
+        w = size
+    else:
+        scale = size / h
+        w = w * scale
+        h = size
+    interpolation = interpolation_up if scale > 1 else interpolation_down
+    resized = cv2.resize(img, (int(w), int(h)), interpolation=interpolation)
+    return resized
+
+
+class IsotropicResize(DualTransform):
+    def __init__(self, max_side, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC,
+                 always_apply=False, p=1):
+        super(IsotropicResize, self).__init__(always_apply, p)
+        self.max_side = max_side
+        self.interpolation_down = interpolation_down
+        self.interpolation_up = interpolation_up
+
+    def apply(self, img, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC, **params):
+        return isotropically_resize_image(img, size=self.max_side, interpolation_down=interpolation_down,
+                                          interpolation_up=interpolation_up)
+
+    def apply_to_mask(self, img, **params):
+        return self.apply(img, interpolation_down=cv2.INTER_NEAREST, interpolation_up=cv2.INTER_NEAREST, **params)
+
+    def get_transform_init_args_names(self):
+        return ("max_side", "interpolation_down", "interpolation_up")
+
+
 def get_aug(img_arr):
-    trans = A.Compose([
-            A.OneOf([
-                A.RandomGamma(gamma_limit=(60, 120), p=0.9),
-                A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.9),
-                A.CLAHE(clip_limit=4.0, tile_grid_size=(4, 4), p=0.9),
-                A.GaussianBlur(),
-            ]),
-            A.HorizontalFlip(p=0.5),
-            A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=20,
-                                interpolation=cv2.INTER_LINEAR, border_mode=cv2.BORDER_CONSTANT, p=1),
-            A.ImageCompression(quality_lower=60, quality_upper=90, p=0.5),
-            A.OneOf([
-                    A.CoarseDropout(),
-                    A.GridDistortion(),
-                    A.GridDropout(),
-                    A.OpticalDistortion()
-                    ]),
-    ])
-    # size = img_arr.shape[0]
     # trans = A.Compose([
-    #         A.ImageCompression(quality_lower=60, quality_upper=100, p=0.5),
-    #         A.Downscale(scale_min=0.7, scale_max=0.9, interpolation=cv2.INTER_LINEAR, p=0.3),
-    #         A.GaussNoise(p=0.1),
-    #         A.GaussianBlur(blur_limit=3, p=0.05),
-    #         A.HorizontalFlip(),
     #         A.OneOf([
-    #             IsotropicResize(max_side=size, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC),
-    #             IsotropicResize(max_side=size, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_LINEAR),
-    #             IsotropicResize(max_side=size, interpolation_down=cv2.INTER_LINEAR, interpolation_up=cv2.INTER_LINEAR),
-    #         ], p=1),
-    #         A.PadIfNeeded(min_height=size, min_width=size, border_mode=cv2.BORDER_CONSTANT),
-    #         A.OneOf([A.RandomBrightnessContrast(), A.FancyPCA(), A.HueSaturationValue()], p=0.7),
-    #         A.ToGray(p=0.2),
-    #         A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=10, border_mode=cv2.BORDER_CONSTANT, p=0.5),
-            
+    #             A.RandomGamma(gamma_limit=(60, 120), p=0.9),
+    #             A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.9),
+    #             A.CLAHE(clip_limit=4.0, tile_grid_size=(4, 4), p=0.9),
+    #             A.GaussianBlur(),
+    #         ]),
+    #         A.HorizontalFlip(p=0.5),
+    #         A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=20,
+    #                             interpolation=cv2.INTER_LINEAR, border_mode=cv2.BORDER_CONSTANT, p=1),
+    #         A.ImageCompression(quality_lower=60, quality_upper=90, p=0.5),
     #         A.OneOf([
     #                 A.CoarseDropout(),
     #                 A.GridDistortion(),
     #                 A.GridDropout(),
     #                 A.OpticalDistortion()
     #                 ]),
-    # ]
-    # )
+    # ])
+    size = img_arr.shape[0]
+    trans = A.Compose([
+            A.ImageCompression(quality_lower=60, quality_upper=100, p=0.5),
+            A.Downscale(scale_min=0.7, scale_max=0.9, interpolation=cv2.INTER_LINEAR, p=0.3),
+            A.GaussNoise(p=0.1),
+            A.GaussianBlur(blur_limit=3, p=0.05),
+            A.HorizontalFlip(),
+            A.OneOf([
+                IsotropicResize(max_side=size, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_CUBIC),
+                IsotropicResize(max_side=size, interpolation_down=cv2.INTER_AREA, interpolation_up=cv2.INTER_LINEAR),
+                IsotropicResize(max_side=size, interpolation_down=cv2.INTER_LINEAR, interpolation_up=cv2.INTER_LINEAR),
+            ], p=1),
+            A.PadIfNeeded(min_height=size, min_width=size, border_mode=cv2.BORDER_CONSTANT),
+            A.OneOf([A.RandomBrightnessContrast(), A.FancyPCA(), A.HueSaturationValue()], p=0.7),
+            A.ToGray(p=0.2),
+            A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.2, rotate_limit=10, border_mode=cv2.BORDER_CONSTANT, p=0.5),
+            
+            # A.OneOf([
+            #         A.CoarseDropout(),
+            #         A.GridDistortion(),
+            #         A.GridDropout(),
+            #         A.OpticalDistortion()
+            #         ]),
+    ]
+    )
     trans_img = trans(image=img_arr)['image']
     return trans_img
 
@@ -136,6 +175,7 @@ class CelebDataset(data.Dataset):
         self.data_root = dataset_root
         self.frame_num = frame_num
         self.train_list = self.collect_image(self.data_root)
+        self.augment = augment
         if augment:
             self.transform = trans.Compose([
                 trans.RandomHorizontalFlip(p=0.5),
@@ -219,15 +259,25 @@ def evaluate(model, data_path, mode='val'):
     # dataset_fake, _ = get_dataset(name=mode, root=origin_root, size=256, frame_num=50, augment=False, fake_list=['Deepfakes', 'Face2Face', 'FaceSwap', 'NeuralTextures'])
     # dataset_img = torch.utils.data.ConcatDataset([dataset_real, dataset_fake])
     # *** celeb *** #
-    dataset_real = CelebDataset(dataset_root=real_root, size=256, frame_num=50, augment=False)
-    dataset_fake = CelebDataset(dataset_root=fake_root, size=256, frame_num=50, augment=False)
+    dataset_real = CelebDataset(dataset_root=real_root, size=256, frame_num=50, augment=True)
+    dataset_fake = CelebDataset(dataset_root=fake_root, size=256, frame_num=50, augment=True)
     dataset_img = torch.utils.data.ConcatDataset([dataset_real, dataset_fake])
 
-    bz = 64
-    # torch.cache.empty_cache()
+    # TTA
+    transforms = tta.Compose(
+    [
+        tta.HorizontalFlip(),
+        # tta.Rotate90(angles=[0, 180]),
+        # tta.Scale(scales=[1, 2]),        
+    ]
+    )
+    tta_model = tta.ClassificationTTAWrapper(model, transforms)
+
+    TTA = True
+
     with torch.no_grad():
         y_true, y_pred = [], []
-
+        bz = 64
         for i, d in enumerate(dataset_img.datasets):
             dataloader = torch.utils.data.DataLoader(
                 dataset = d,
@@ -241,9 +291,24 @@ def evaluate(model, data_path, mode='val'):
                 if not label.any() == 0:
                     label = torch.ones_like(label)
                 img = img.detach().cuda()
-                output = model.forward(img, train=False)
-                y_pred.extend(output[1].sigmoid().flatten().tolist())  # 0:spe, 1:sha
-                y_true.extend(label.flatten().tolist())
+                if not TTA:
+                    # *** normal test *** #
+                    output = model.forward(img, train=False)
+                    y_pred.extend(output[1].sigmoid().flatten().tolist())  # 0:spe, 1:sha
+                    y_true.extend(label.flatten().tolist())
+                else:
+                    # *** TTA *** #
+                    tmp_pred, tmp_true = [], []
+                    for transformer in transforms: # custom transforms or e.g. tta.aliases.d4_transform() 
+                        # augment image
+                        augmented_image = transformer.augment_image(img)
+                        # pass to model
+                        output = model.forward(augmented_image, False)
+                        tmp_pred.extend(output[1].sigmoid().flatten().tolist())  # 0:spe, 1:sha
+                        tmp_true.extend(label.flatten().tolist())
+                    y_pred.extend([np.mean(x) for x in chunked(tmp_pred, 2)])
+                    y_true.extend([np.mean(x) for x in chunked(tmp_true, 2)])
+                    torch.cuda.empty_cache()
 
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     fpr, tpr, thresholds = roc_curve(y_true,y_pred,pos_label=1)
