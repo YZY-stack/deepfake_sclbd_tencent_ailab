334616bcd6e8208547da155980a77e6c3be46d05
steven.yan <75616468+YZY-stack@users.noreply.github.com>
2022-12-03 10:18:18 +0800

    Add visualization for forgery_image and reconstruction image

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import sys
 import time
 import torch
 import torch.nn
+from torchvision import utils as vutils
 
 from utils import evaluate, get_dataset, FFDataset, setup_logger
 from trainer import Trainer
@@ -20,9 +21,12 @@ batch_size = 32
 gpu_ids = [*range(osenvs)]
 max_epoch = 15
 loss_freq = 40
+visualization = False
 mode = 'FAD' # ['Original', 'FAD', 'LFS', 'Both', 'Mix']
 ckpt_dir = './weights'
-ckpt_name = 'paper_disfin_notta_bs32_lr00005'
+ckpt_name = 'paper_disfin_notta_bs32_lr00002_mlp_addlambda_imgshow_new'
+img_save_path = 'img_save'
+os.makedirs(img_save_path, exist_ok=True)
 
 
 if __name__ == '__main__':
@@ -109,7 +113,12 @@ if __name__ == '__main__':
             model.set_input(data,label)
 
             stu_fea, stu_cla = model.model(model.input, pair_index=pair_index)
-            (spe_out, sha_out), reconstruction_image_1, reconstruction_image_2, forgery_image_12 = stu_cla
+            (spe_out, sha_out), \
+            reconstruction_image_1, \
+            reconstruction_image_2, \
+            forgery_image_12, \
+            f1_spe, f3_spe \
+            = stu_cla
 
             # *** share label task *** #
             # model.label = label.to(model.device)
@@ -124,20 +133,66 @@ if __name__ == '__main__':
             model.label = spe_label.to(model.device)
             loss_specific = model.optimize_weight_ce(spe_out)
 
-            # *** mse construction task *** #
-            loss_reconstruction = model.optimize_weight_mse(
+            # *** mse construction task *** # 
+            loss_reconstruction_1, \
+            loss_reconstruction_2, \
+            loss_reconstruction_3, \
+            loss_f1_f3 \
+                = model.optimize_weight_mse(
                 data_real.to(model.device),
                 data_fake.to(model.device),
                 reconstruction_image_1,
                 reconstruction_image_2,
                 forgery_image_12,
+                f1_spe, 
+                f3_spe,
             )
 
+            loss_reconstruction = loss_reconstruction_1 + \
+                                  loss_reconstruction_2 + \
+                                  loss_reconstruction_3 + \
+                                  loss_f1_f3
+
             # total loss
-            loss = model.get_final_loss(loss_share, loss_specific, loss_reconstruction)
+            loss = model.get_final_loss(
+                loss_share, 
+                loss_specific,
+                loss_reconstruction
+            )
 
             if model.total_steps % loss_freq == 0:
-                logger.debug(f'loss: {loss:.5f}, spe_loss: {loss_specific:.5f}, sha_loss: {loss_share:.5f}, mse_loss: {loss_reconstruction:.5f} at step: {model.total_steps}')
+                logger.debug(
+                    f'loss: {loss:.5f}' 
+                    f'spe_loss: {loss_specific:.5f}' 
+                    f'sha_loss: {loss_share:.5f}' 
+                    f'mse_loss: {loss_reconstruction:.5f}' 
+                    f'loss_fake_reconstruction1: {loss_reconstruction_1:.5f}' 
+                    f'loss_real_reconstruction2: {loss_reconstruction_2:.5f}' 
+                    f'loss_forgery_real: {loss_reconstruction_3:.5f}' 
+                    f'loss_f1_f3_spe: {loss_f1_f3:.5f}' 
+                    f'at step: {model.total_steps}'
+                )
+                # save img
+                if visualization:
+                    vutils.save_image(
+                        reconstruction_image_1.detach().cpu(), 
+                        f'{img_save_path}/epoch{epoch}_step{model.total_steps}_reconstruction_image_1.png')
+
+                    vutils.save_image(
+                        reconstruction_image_2.detach().cpu(), 
+                        f'{img_save_path}/epoch{epoch}_step{model.total_steps}_reconstruction_image_2.png')
+
+                    vutils.save_image(
+                        forgery_image_12.detach().cpu(), 
+                        f'{img_save_path}/epoch{epoch}_step{model.total_steps}_forgery_image_12.png')
+
+                    vutils.save_image(
+                        data_real.detach().cpu(), 
+                        f'{img_save_path}/epoch{epoch}_step{model.total_steps}_data_real.png')
+                        
+                    vutils.save_image(
+                        data_fake.detach().cpu(), 
+                        f'{img_save_path}/epoch{epoch}_step{model.total_steps}_data_fake.png')
 
             if i % int(len_dataloader / 10) == 0:
                 model.model.eval()
