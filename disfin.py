5ba74b6927cc3861f36d91402769a6dc6e6e7e84
steven.yan <75616468+YZY-stack@users.noreply.github.com>
2022-12-03 10:38:33 +0800

    Add mlp for block features extraction

--- disfin.py
+++ disfin.py
@@ -27,6 +27,19 @@ class MLP(nn.Module):
         x = self.mlp(x)
         return x
 
+class Conv2d1x1(nn.Module):
+    def __init__(self, in_f, hidden_dim, out_f):
+        super(Conv2d1x1, self).__init__()
+        self.conv2d = nn.Sequential(nn.Conv2d(in_f, hidden_dim, 1, 1),
+                                nn.LeakyReLU(inplace=True),
+                                nn.Conv2d(hidden_dim, hidden_dim, 1, 1),
+                                nn.LeakyReLU(inplace=True),
+                                nn.Conv2d(hidden_dim, out_f, 1, 1),)
+
+    def forward(self, x):
+        x = self.conv2d(x)
+        return x
+
 class Head(torch.nn.Module):
   def __init__(self, in_f, hidden_dim, out_f):
     super(Head, self).__init__()
@@ -48,23 +61,23 @@ class Head(torch.nn.Module):
     return x
 
 class disfin(nn.Module):
-    def __init__(self, num_classes, img_size, encoder_feat_dim) -> None:
+    def __init__(self, num_classes, encoder_feat_dim) -> None:
         super(disfin, self).__init__()
         # init variable
         self.num_classes = num_classes
-        self.img_size = img_size
         self.encoder_feat_dim = encoder_feat_dim
         self.half_fingerprint_dim = encoder_feat_dim//2
         
         # basic function
         self.lr = nn.LeakyReLU(inplace=True)
         self.do = nn.Dropout(0.2)
+        self.pool = nn.AdaptiveAvgPool2d(1)
 
         # models
-        # self.encoder = self.init_xcep()  # xception
-        self.encoder = self.init_resnest()  # resnest
+        self.encoder = self.init_xcep()  # xception
+        # self.encoder = self.init_resnest()  # resnest
         # self.encoder = self.init_efficient()  # efficient
-        self.encoder = self.init_convnext()  # convnext
+        # self.encoder = self.init_convnext()  # convnext
 
 
         self.con_gan = CUNet(2)
@@ -74,16 +87,11 @@ class disfin(nn.Module):
         self.head_spe = Head(in_f=256, hidden_dim=512, out_f=5)
         self.head_sha = Head(in_f=256, hidden_dim=512, out_f=1)
 
-        # self.block_fake_fin = Head(in_f=1024, hidden_dim=512, out_f=512)
-        # self.block_fake_con = Head(in_f=1024, hidden_dim=512, out_f=512)
+        self.block_fin = Conv2d1x1(in_f=1024, hidden_dim=512, out_f=512)
+        self.block_con = Conv2d1x1(in_f=1024, hidden_dim=512, out_f=512)
         
-        # self.block_real_fin = Head(in_f=1024, hidden_dim=512, out_f=512)
-        # self.block_real_con = Head(in_f=1024, hidden_dim=512, out_f=512)
-
-        # self.block_fake_spe = Head(in_f=512, hidden_dim=256, out_f=256)
-        # self.block_fake_sha = Head(in_f=512, hidden_dim=256, out_f=256)
-        # self.block_real_spe = Head(in_f=512, hidden_dim=256, out_f=256)
-        # self.block_real_sha = Head(in_f=512, hidden_dim=256, out_f=256)
+        self.block_spe = Conv2d1x1(in_f=512, hidden_dim=256, out_f=256)
+        self.block_sha = Conv2d1x1(in_f=512, hidden_dim=256, out_f=256)
 
     def init_xcep(self, pretrained_path='pretrained/xception-b5690688.pth'):
         xcep = Xception(self.num_classes)
@@ -122,8 +130,8 @@ class disfin(nn.Module):
 
     def forward(self, cat_data, train=True, pair_index=0):
         bs = cat_data.shape[0]
-        # hidden = self.encoder.features(cat_data)  # -> bs,1024
-        hidden = self.encoder.forward_features(cat_data)  # -> bs,1024
+        hidden = self.encoder.features(cat_data)  # -> bs,1024
+        # hidden = self.encoder.forward_features(cat_data)  # -> bs,1024
         # out = self.head(hidden)
         hidden_real = hidden[:bs//2, :, :, :]
         hidden_fake = hidden[bs//2:, :, :, :]
@@ -131,21 +139,21 @@ class disfin(nn.Module):
         # out_sha = self.head_sha(hidden)
         # out = (out_spe, out_sha)
 
-        c1, f1 = hidden_fake[:, :self.encoder_feat_dim, :, :], hidden_fake[:, self.encoder_feat_dim:, :, :]
-        f1_spe, f1_share = f1[:, :self.half_fingerprint_dim, :, :], f1[:, self.half_fingerprint_dim:, :, :]
+        # c1, f1 = hidden_fake[:, :self.encoder_feat_dim, :, :], hidden_fake[:, self.encoder_feat_dim:, :, :]
+        # f1_spe, f1_share = f1[:, :self.half_fingerprint_dim, :, :], f1[:, self.half_fingerprint_dim:, :, :]
 
-        c2, f2 = hidden_real[:, :self.encoder_feat_dim, :, :], hidden_real[:, self.encoder_feat_dim:, :, :]
-        f2_spe, f2_share = f2[:, :self.half_fingerprint_dim, :, :], f2[:, self.half_fingerprint_dim:, :, :]
+        # c2, f2 = hidden_real[:, :self.encoder_feat_dim, :, :], hidden_real[:, self.encoder_feat_dim:, :, :]
+        # f2_spe, f2_share = f2[:, :self.half_fingerprint_dim, :, :], f2[:, self.half_fingerprint_dim:, :, :]
 
-        # f1 = self.block_fake_fin(hidden_fake).view(bs//2, -1, 1, 1)
-        # c1 = self.block_fake_con(hidden_fake).view(bs//2, -1, 1, 1)
-        # f1_spe = self.block_fake_spe(f1).view(bs//2, -1, 1, 1)
-        # f1_share = self.block_fake_sha(f1).view(bs//2, -1, 1, 1)
+        f1 = self.block_fin(hidden_fake)
+        c1 = self.block_con(hidden_fake)
+        f1_spe = self.block_spe(f1)
+        f1_share = self.block_sha(f1)
 
-        # f2 = self.block_real_fin(hidden_real).view(bs//2, -1, 1, 1)
-        # c2 = self.block_real_con(hidden_real).view(bs//2, -1, 1, 1)
-        # f2_spe = self.block_real_spe(f2).view(bs//2, -1, 1, 1)
-        # f2_share = self.block_real_sha(f2).view(bs//2, -1, 1, 1)
+        f2 = self.block_fin(hidden_real)
+        c2 = self.block_con(hidden_real)
+        f2_spe = self.block_spe(f2)
+        f2_share = self.block_sha(f2)
 
         if train:
             # pair combination
@@ -153,10 +161,10 @@ class disfin(nn.Module):
             if pair_index == 0:
                 # reconstruction mse loss
                 forgery_image_12 = self.con_gan(f1, c2)
-                # hidden_fake_plus = self.encoder.features(forgery_image_12)
-                hidden_fake_plus = self.encoder.forward_features(forgery_image_12)
-                c3, f3 = hidden_fake_plus[:, :self.encoder_feat_dim, :, :], hidden_fake[:, self.encoder_feat_dim:, :, :]
-                f3_spe, f3_share = f3[:, :self.half_fingerprint_dim, :, :], f2[:, self.half_fingerprint_dim:, :, :]
+                hidden_fake_plus = self.encoder.features(forgery_image_12)
+                # hidden_fake_plus = self.encoder.forward_features(forgery_image_12)
+                c3, f3 = hidden_fake_plus[:, :self.encoder_feat_dim, :, :], hidden_fake_plus[:, self.encoder_feat_dim:, :, :]
+                f3_spe, f3_share = f3[:, :self.half_fingerprint_dim, :, :], f3[:, self.half_fingerprint_dim:, :, :]
 
                 reconstruction_image_1 = self.con_gan(f3, c1)
                 reconstruction_image_2 = self.con_gan(f2, c3)
@@ -165,8 +173,8 @@ class disfin(nn.Module):
             else:
                 # reconstruction mse loss
                 forgery_image_12 = self.con_gan(f2, c1)
-                # hidden_fake_plus = self.encoder.features(forgery_image_12)
-                hidden_fake_plus = self.encoder.forward_features(forgery_image_12)
+                hidden_fake_plus = self.encoder.features(forgery_image_12)
+                # hidden_fake_plus = self.encoder.forward_features(forgery_image_12)
                 c3, f3 = hidden_fake_plus[:, :self.encoder_feat_dim, :, :], hidden_fake[:, self.encoder_feat_dim:, :, :]
                 f3_spe, f3_share = f3[:, :self.half_fingerprint_dim, :, :], f2[:, self.half_fingerprint_dim:, :, :]
 
@@ -178,9 +186,13 @@ class disfin(nn.Module):
             # out_sha = self.head_sha(torch.cat((f2_share, f1_share, f3_share), dim=0))
             out_spe = self.head_spe(torch.cat((f2_spe, f1_spe), dim=0))
             out_sha = self.head_sha(torch.cat((f2_share, f1_share), dim=0))
+            # out_sha = self.head_sha(torch.cat((f2_share, f1_share, f3_share), dim=0))
             out = (out_spe, out_sha)
 
-            return None, (out, reconstruction_image_1, reconstruction_image_2, forgery_image_12)
+            f3_spe = self.pool(f3_spe)
+            f1_spe = self.pool(f1_spe)
+
+            return None, (out, reconstruction_image_1, reconstruction_image_2, forgery_image_12, f1_spe, f3_spe)
         
         # inference only consider share loss
         else:
