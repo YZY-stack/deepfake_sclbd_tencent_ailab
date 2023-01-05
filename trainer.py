dcceb4e48ea43d2a0fa60a971e6b2e35964ca989
steven.yan <75616468+YZY-stack@users.noreply.github.com>
2023-01-06 01:37:16 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -34,7 +34,7 @@ class Trainer():
         self.discriminator.to(self.device)
 
         # loss
-        self.loss_fn = nn.BCEWithLogitsLoss()
+        # self.loss_fn = nn.CrossEntropyLoss()
         self.loss_fn_ce = nn.CrossEntropyLoss()
         # self.adv_loss = nn.CrossEntropyLoss()
         self.adv_loss = nn.MSELoss()  # lsgan
@@ -48,15 +48,15 @@ class Trainer():
         )
         self.optimizer_d = torch.optim.Adam(
             filter(lambda p: p.requires_grad, self.discriminator.parameters()),
-            lr=0.0002, weight_decay=0, betas=(0.9, 0.999)
+            lr=0.0001, weight_decay=0, betas=(0.9, 0.999)
         )
         # self.optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.model.parameters()),
         #                                         lr=0.002, momentum=0.9, weight_decay=0)
         # self.scheduler = CosineAnnealingWarmRestarts(self.optimizer, T_0=3, T_mult=2, eta_min=1e-6, last_epoch=-1)
         
         self.scheduler = CosineWarmupLr(
-                            self.optimizer, 32, 15,
-                            base_lr=0.0002, warmup_epochs=3
+                            self.optimizer, 32, 20,
+                            base_lr=0.0002, warmup_epochs=4
                         )
 
     def set_input(self, input, label):
@@ -127,10 +127,10 @@ class Trainer():
 
         # # feature reconstruction loss
         # # cross
-        # loss_f1_f12 = self.loss_fn_mse(f1, f12)
-        # loss_f2_f21 = self.loss_fn_mse(f2, f21)
-        # loss_c2_c12 = self.loss_fn_mse(c2, c12)
-        # loss_c1_c21 = self.loss_fn_mse(c1, c21)
+        # # loss_f1_f12 = self.loss_fn_mse(f1, f12)
+        # # loss_f2_f21 = self.loss_fn_mse(f2, f21)
+        # # loss_c2_c12 = self.loss_fn_mse(c2, c12)
+        # # loss_c1_c21 = self.loss_fn_mse(c1, c21)
         # # within
         # loss_f1_f1prime = self.loss_fn_mse(f1, f1_recon)
         # loss_c1_c1prime = self.loss_fn_mse(c1, c1_recon)
@@ -139,8 +139,8 @@ class Trainer():
 
         # feature_loss = \
         #     loss_f1_f1prime + loss_c1_c1prime + \
-        #     loss_f2_f2prime + loss_c2_c2prime + \
-        #     loss_f1_f12 + loss_f2_f21 + loss_c2_c12 + loss_c1_c21
+        #     loss_f2_f2prime + loss_c2_c2prime
+        #     # loss_f1_f12 + loss_f2_f21 + loss_c2_c12 + loss_c1_c21
 
         self.loss_reconstruction =  \
             self_loss_reconstruction_1 + \
@@ -170,6 +170,8 @@ class Trainer():
             # forgery_image_21
         ), dim=0)
         adv = self.discriminator(data)
+        adv = adv.view(-1)
+        adv_label = adv_label.view(-1)
         self.loss_discriminator = self.adv_loss(adv, adv_label.float())  # real,fake,real,fake
         # self.loss_discriminator = self.adv_loss(adv, adv_label.long())  # real,fake,real,fake
 
@@ -205,18 +207,21 @@ class Trainer():
         loss_spe, 
         loss_reconstruction,
         # feature_loss,
+        perceptual_loss,
         loss_discriminator,
         lambda_sha=1,
         lambda_spe=1,
         lambda_mse=0.3,
         # lambda_feat=0.5,
+        lambda_per=0.01,
         lambda_adv=0.01,
     ):
         self.loss = \
             lambda_sha  * loss_sha + \
             lambda_spe  * loss_spe + \
             lambda_mse  * loss_reconstruction + \
-            lambda_adv  * loss_discriminator
+            lambda_adv  * loss_discriminator + \
+            lambda_per  * perceptual_loss
             # lambda_feat * feature_loss 
         
         self.optimizer.zero_grad()
